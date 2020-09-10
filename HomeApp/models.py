2e52b9080cf8d7e082329a299a3dcac0c7ae8eb5
Liubin <951543516@qq.com>
2020-09-10 19:42:31 +0800

    创建循环播放图片的数据库

--- HomeApp/models.py
+++ HomeApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class BlmWheel(models.Model):
+    img = models.CharField(max_length=64)
+    name = models.CharField(max_length=32)
+    trackid = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'blm_wheel'
--- HomeApp/views.py
+++ HomeApp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from HomeApp.models import BlmWheel
+
+
 def home(request):
-    return render(request, 'blm/main/home/home.html')
-
-
+    wheels = BlmWheel.objects.all()
+    context = {
+        'wheels': wheels
+    }
+    return render(request, 'blm/main/home/home.html', context=context)
