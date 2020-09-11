71cf4282e1e4f4880da7e703fba24c514dc3e305
Liubin <951543516@qq.com>
2020-09-11 11:50:01 +0800

    App主页面 图片模型的构建

--- HomeApp/models.py
+++ HomeApp/models.py
@@ -1,11 +1,53 @@
 from django.db import models
 
 
-# Create your models here.
-class BlmWheel(models.Model):
+# 主页面的图片 模型
+class HomeImg(models.Model):
     img = models.CharField(max_length=64)
     name = models.CharField(max_length=32)
     trackid = models.IntegerField(null=True, blank=True)
 
     class Meta:
+        abstract = True
+
+
+# 图片循环播放 模型
+class BlmWheel(HomeImg):
+    class Meta:
         db_table = 'blm_wheel'
+
+
+# 选择功能图片 模型
+class BlmChoice(HomeImg):
+    class Meta:
+        db_table = 'blm_choice'
+
+
+# 优惠券 模型
+class BlmDiscount(HomeImg):
+    class Meta:
+        db_table = 'blm_discount'
+
+
+# 品牌店 模型
+class BlmBrand(HomeImg):
+    class Meta:
+        db_table = 'blm_brand'
+
+
+# 限时活动 模型
+class BlmActivity(HomeImg):
+    img2 = models.CharField(max_length=64, null=True, blank=True)
+
+    class Meta:
+        db_table = 'blm_activity'
+
+
+# 专属推荐 模型
+class BlmRecommend(HomeImg):
+    status = models.CharField(max_length=16, null=True, blank=True)
+    now_price = models.IntegerField()
+    ex_price = models.IntegerField()
+
+    class Meta:
+        db_table = 'blm_recommend'
--- HomeApp/views.py
+++ HomeApp/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
 from HomeApp.models import BlmWheel
+from HomeApp.models import BlmChoice
+from HomeApp.models import BlmDiscount
+from HomeApp.models import BlmBrand
+from HomeApp.models import BlmActivity
+from HomeApp.models import BlmRecommend
 
 
 def home(request):
     wheels = BlmWheel.objects.all()
+    choices = BlmChoice.objects.all()
+    discounts = BlmDiscount.objects.all()
+    brands = BlmBrand.objects.all()
+    activities = BlmActivity.objects.all()
+    recommends = BlmRecommend.objects.all()
+
     context = {
-        'wheels': wheels
+        'wheels': wheels,
+        'choices': choices,
+        'discounts': discounts,
+        'brands': brands,
+        'activities': activities,
+        'recommends': recommends,
     }
     return render(request, 'blm/main/home/home.html', context=context)
